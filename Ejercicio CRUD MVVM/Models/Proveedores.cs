a0b9918b30cf1e470411eeb74a9821e29cf8f007
Kevin Chacon <kevin27chacon@gmail.com>
2024-09-15 17:51:58 -0600

    creacion de carpetas y archivo sql

---
+++ Ejercicio CRUD MVVM/Models/Proveedores.cs
@@ -0,0 +1,20 @@
+﻿using SQLite;
+
+namespace Ejercicio_CRUD_MVVM.Models
+{
+    public class Proveedores
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [NotNull]
+        public string Nombre { get; set; }
+
+        public string Direccion { get; set; }
+
+        public string Telefono { get; set; }
+
+        public string Email {  get; set; }
+
+    }
+}
---
+++ Ejercicio CRUD MVVM/Services/ProveedoresService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using Ejercicio_CRUD_MVVM.Models;
+
+namespace Ejercicio_CRUD_MVVM.Services
+{
+    public class ProveedoresService
+    {
+        private readonly SQLiteConnection dbConnection;
+
+        public ProveedoresService()
+        {
+
+        }
+    }
+}
